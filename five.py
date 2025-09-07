3f16116b4fb6e92ea0480df2406d09b51ce57c30
fengning <1083566157@qq.com>
2025-09-07 14:27:18 +0800

    update five.py file

--- five.py
+++ five.py
@@ -1,4 +1,11 @@
+import time
 this is new five
 
 for i in range(5):
     print(i)
+
+T1 = time.time()
+interval_time = time.time() - T1
+print("This code block took {} seconds".format(interval_time))
+
+
